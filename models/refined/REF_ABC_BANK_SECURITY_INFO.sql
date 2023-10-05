da9aa6f82af6924441e9f3558b425ac8784a4aef
sai sankar reddy mannem <learncode721@gmail.com>
2023-10-05 11:39:26 +0000

    added two macros. i.e., current_from_snapshot and to_21st_century_date

--- models/refined/REF_ABC_BANK_SECURITY_INFO.sql
+++ models/refined/REF_ABC_BANK_SECURITY_INFO.sql
@@ -1,7 +1,9 @@
 with current_from_snapshot as (
-    select * EXCLUDE (DBT_SCD_ID, DBT_UPDATED_AT,
-                           DBT_VALID_FROM, DBT_VALID_TO) 
-    from {{ref("SNSH_ABC_BANK_SECURITY_INFO")}}
-    where DBT_VALID_TO is null
+    {{
+        current_from_snapshot(
+            snsh_ref=ref("SNSH_ABC_BANK_SECURITY_INFO"),
+            output_load_ts = false
+        )
+    }}
 )
 select * from current_from_snapshot
